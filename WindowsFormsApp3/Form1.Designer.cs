d50014d8af76b8c3d9801e18e10813d594a8d4f1
301044427 <301044427@PR-E216-13>
2019-06-26 18:00:51 -0400

    completed initial version lab08

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -28,15 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.TITLELABLE = new System.Windows.Forms.Label();
             this.INFOGROUPBOX = new System.Windows.Forms.GroupBox();
-            this.NAME = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NAME = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.outputlabel = new System.Windows.Forms.Label();
             this.INFOGROUPBOX.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,8 +54,6 @@
             // INFOGROUPBOX
             // 
             this.INFOGROUPBOX.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.INFOGROUPBOX.Controls.Add(this.label3);
-            this.INFOGROUPBOX.Controls.Add(this.textBox2);
             this.INFOGROUPBOX.Controls.Add(this.label2);
             this.INFOGROUPBOX.Controls.Add(this.textBox1);
             this.INFOGROUPBOX.Controls.Add(this.label1);
@@ -66,25 +65,6 @@
             this.INFOGROUPBOX.TabIndex = 1;
             this.INFOGROUPBOX.TabStop = false;
             this.INFOGROUPBOX.Text = "INFO";
-            // 
-            // NAME
-            // 
-            this.NAME.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NAME.Location = new System.Drawing.Point(269, 20);
-            this.NAME.Name = "NAME";
-            this.NAME.Size = new System.Drawing.Size(258, 38);
-            this.NAME.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(219, 31);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "ENTER NAME :";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // label2
             // 
@@ -102,47 +82,67 @@
             this.textBox1.Location = new System.Drawing.Point(269, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(258, 38);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 116);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(219, 31);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "ENTER NAME :";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ENTER NAME :";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
-            // textBox2
+            // NAME
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(269, 109);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 38);
-            this.textBox2.TabIndex = 6;
+            this.NAME.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NAME.Location = new System.Drawing.Point(269, 20);
+            this.NAME.Name = "NAME";
+            this.NAME.Size = new System.Drawing.Size(258, 38);
+            this.NAME.TabIndex = 1;
+            this.NAME.TextChanged += new System.EventHandler(this.NAME_TextChanged);
             // 
-            // button1
+            // submit
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(630, 398);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 50);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "SUBMIT";
-            this.button1.UseVisualStyleBackColor = false;
+            this.submit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(630, 388);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(150, 50);
+            this.submit.TabIndex = 3;
+            this.submit.Text = "SUBMIT";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // outputlabel
+            // 
+            this.outputlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputlabel.Location = new System.Drawing.Point(33, 388);
+            this.outputlabel.Name = "outputlabel";
+            this.outputlabel.Size = new System.Drawing.Size(591, 48);
+            this.outputlabel.TabIndex = 6;
+            this.outputlabel.Click += new System.EventHandler(this.outputlabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputlabel);
+            this.Controls.Add(this.submit);
             this.Controls.Add(this.INFOGROUPBOX);
             this.Controls.Add(this.TITLELABLE);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.INFOGROUPBOX.ResumeLayout(false);
             this.INFOGROUPBOX.PerformLayout();
             this.ResumeLayout(false);
@@ -155,11 +155,11 @@
         private System.Windows.Forms.GroupBox INFOGROUPBOX;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox NAME;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label outputlabel;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        string username { get; set; }
+        float age { get; set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +26,69 @@ namespace WindowsFormsApp3
         }
 
         private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NAME_TextChanged(object sender, EventArgs e)
+        {
+
+            submit.Enabled = (NAME.Text.Length >= 2) ? true : false;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float.Parse(textBox1.Text);
+                submit.Enabled = true;
+            }
+            catch 
+            {
+                submit.Enabled = false;
+            }
+
+
+            
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            submit.Enabled = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            username = NAME.Text;
+
+            //OPTION1
+            //age = Convert.ToSingle(textBox1.Text);
+
+            //OPTION2
+            age = float.Parse(textBox1.Text);
+
+            ////option3
+            //float tempfloat;
+            //bool result = float.TryParse(textBox1.Text, out tempfloat);
+            //age = tempfloat;
+
+
+            outputlabel.Text = NAME.Text + " " + textBox1.Text;
+
+            clearform();
+        }
+
+        private void clearform()
+        {
+            NAME.Clear();
+            textBox1.Clear();
+
+        }
+
+        private void outputlabel_Click(object sender, EventArgs e)
         {
 
         }
